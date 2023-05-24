16012500b779fc9e9b69b6296b7cccd090134aaf
Manuel Vazquez <manuel.vazquez90@gmail.com>
2023-05-24 16:46:02 +0000

    cloudwatch logging

--- api/app.py
+++ api/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import request
+from time import strftime
 
 from services.games import *
+
+# CloudWatch - Imports
+import watchtower
+import logging
 
 # Honeycomb - Imports
 from opentelemetry import trace
@@ -20,6 +26,15 @@ tracer = trace.get_tracer(__name__)
 # Initialize Flask App
 app = Flask(__name__)
 
+# CloudWatch - Configuration
+#TODO: get log_group from flask config
+console_handler = logging.StreamHandler()
+cloudwatch_handler = watchtower.CloudWatchLogHandler(log_group='nba-squeeze-api')
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cloudwatch_handler)
+
 # Honeycomb - Instrumentation
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
@@ -28,6 +43,12 @@ RequestsInstrumentor().instrument()
 def data_games():
     data = Games.run()
     return data, 200
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%m-%d %H:%M]')
+    LOGGER.info('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
